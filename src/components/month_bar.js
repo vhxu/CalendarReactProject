a592fb1282bc37181a8f3d79831082fd5d16ab8a
Virgil Xu <virgilxu89@gmail.com>
2017-10-24 00:56:56 -0700

    added state and ability to go back in forth in months

--- src/components/month_bar.js
+++ src/components/month_bar.js
@@ -1,20 +1,46 @@
 import React, { Component } from 'react';
 
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var number = new Date().getMonth();
+
 class MonthBar extends Component {
-  getMonth() {
-    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const currentMonth = new Date().getMonth();
-    return (
-      monthNames[currentMonth]
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      month: ""
+
+      // monthNumber: new Date().getMonth(),
+      // month: this.selectMonth(this.monthNumber)
+    };
+  }
+
+
+  componentDidMount () {
+    this.setState({
+      month: this.selectMonth(number)
+    })
+
+  }
+
+  selectMonth(month) {
+    return(
+      monthNames[month]
     );
   }
 
   handleLeftClick() {
-    console.log('left');
+    this.setState({
+      month: this.selectMonth(number + 1)
+    })
+    number += 1;
   }
 
   handleRightClick() {
-    console.log('right');
+    this.setState({
+      month: this.selectMonth(number - 1)
+    })
+    number -= 1;
   }
 
 
@@ -22,9 +48,9 @@ class MonthBar extends Component {
 
     return(
       <div>
-        <h2 onClick={ () => this.handleLeftClick()} >left</h2>
-        <h1>{this.getMonth()}</h1>
-        <h2 onClick={ () => this.handleRightClick() }>right</h2>
+        <h2 onClick={this.handleLeftClick.bind(this)} >left</h2>
+        <h1>{this.state.month}</h1>
+        <h2 onClick={this.handleRightClick.bind(this)}>right</h2>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 
 import MonthBar from './components/month_bar';
 
+
 ReactDOM.render(
   <div>
-    <h1>Hi</h1>
     <MonthBar />
   </div>
   , document.querySelector('.container'));
