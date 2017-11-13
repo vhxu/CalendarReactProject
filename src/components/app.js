6e4f8daedbc6e8c43d8b1bd9d4d35d2b57f5ae2b
Virgil Xu <virgilxu89@gmail.com>
2017-11-12 23:47:16 -0800

    moved input to app.js component

--- src/components/app.js
+++ src/components/app.js
@@ -17,7 +17,8 @@ class App extends Component {
       currentDay: "",
       totalDays: "",
       selectedDay: "",
-      datesArray: ""
+      datesArray: [],
+      tasksArray: []
     };
     this.selectDay = this.selectDay.bind(this);
   }
@@ -29,7 +30,8 @@ class App extends Component {
       currentDay: new Date().getDate(),
       totalDays: this.totalDays(),
       selectedDay: new Date().getDate(),
-      datesArray: []
+      datesArray: [],
+      tasksArray: []
     })
 
   }
@@ -91,6 +93,71 @@ class App extends Component {
       datesArray: []
     })
   }
+  // createInputs() {
+  //   document.getElementById('plusButton').remove();
+  //   var timeInput = document.querySelector('.list-of-stuff').appendChild(document.createElement("input"));
+  //   timeInput.setAttribute('placeholder', 'time');
+  //   timeInput.setAttribute('id', 'time');
+  //   var taskInput = document.querySelector('.list-of-stuff').appendChild(document.createElement("input"));
+  //   taskInput.setAttribute('placeholder', 'task');
+  //   taskInput.setAttribute('id', 'task');
+  //   var addButton = document.querySelector('.list-of-stuff').appendChild(document.createElement("div"));
+  //   addButton.setAttribute('id', 'add');
+  //   addButton.innerHTML = 'Add';
+  //   addButton.onclick = this.inputInfo.bind(this);
+  // }
+  //
+  // inputInfo() {
+  //   var timeInput = document.getElementById("time").value;
+  //   var taskInput = document.getElementById("task").value;
+  //   var splitTime = timeInput.split(':');
+  //   var convertToMinutes = (+splitTime[0])*60 + (+splitTime[1]);
+  //   var taskObject = {date: this.props.month+" "+this.props.selectedDay+", "+this.props.year, time: timeInput, minutes: convertToMinutes, task: taskInput};
+  //   tasksObjectArray.push(taskObject);
+  //   document.getElementById("time").remove();
+  //   document.getElementById("task").remove();
+  //   document.getElementById("add").remove();
+  //   var plusButton = document.querySelector('.list').appendChild(document.createElement("div"));
+  //   plusButton.setAttribute('id', 'plusButton');
+  //   plusButton.className = 'list-add';
+  //   plusButton.innerHTML = '+';
+  //   plusButton.onclick = this.createInputs.bind(this);
+  // }
+
+  createInputs() {
+    console.log('pushed');
+    document.getElementById('plus').remove();
+    var timeInput = document.querySelector('.inputs').appendChild(document.createElement("input"));
+    timeInput.placeholder = 'time';
+    timeInput.id = 'time';
+    var taskInput = document.querySelector('.inputs').appendChild(document.createElement("input"));
+    taskInput.placeholder = 'task';
+    taskInput.id = 'task';
+    var addButton = document.querySelector('.inputs').appendChild(document.createElement("span"));
+    addButton.id = 'add';
+    addButton.innerHTML = 'Add';
+    addButton.onclick = this.inputInfo.bind(this);
+  }
+
+  inputInfo() {
+    var timeInput = document.getElementById("time").value;
+    var taskInput = document.getElementById("task").value;
+    var splitTime = timeInput.split(':');
+    var convertToMinutes = (+splitTime[0])*60 + (+splitTime[1]);
+    var taskObject = {date: this.state.month+" "+this.state.selectedDay+", "+this.state.year, time: timeInput, minutes: convertToMinutes, task: taskInput};
+    this.setState({
+      tasksArray: this.state.tasksArray.concat([taskObject]),
+      datesArray: []
+    })
+    console.log(this.state.tasksArray);
+    document.getElementById("time").remove();
+    document.getElementById("task").remove();
+    document.getElementById("add").remove();
+    var plusButton = document.querySelector('.month-bar').appendChild(document.createElement("div"));
+    plusButton.setAttribute('id', 'plus');
+    plusButton.innerHTML = '+';
+    plusButton.onclick = this.createInputs.bind(this);
+  }
 
   render() {
 
@@ -102,6 +169,8 @@ class App extends Component {
             <div>{this.state.month} {this.state.year}</div>
             <div onClick={this.handleRightClick.bind(this)}>&#8811;</div>
           </div>
+          <div id='plus' onClick={this.createInputs.bind(this)}>+</div>
+          <div className='inputs'></div>
         </div>
         <div className='dates-section'>
           <Dates datesArray={this.state.datesArray} totalDays={this.state.totalDays} month={monthNumber} monthName={this.state.month} year={this.state.year} currentDay={this.state.currentDay} selectedDay={this.state.selectedDay} selectDay = {this.selectDay}/>
